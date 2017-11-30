94bbf3396af137a4bb1bf64419448930240ce065
leeheigun <leeheigun@naver.com>
2017-11-30 09:40:14 +0900

    asd
    asd

--- src/controller/BoardController.java
+++ src/controller/BoardController.java
@@ -88,10 +88,6 @@ public class BoardController {
 				savedName = "noImg";
 			}
 			board.setBoardImg(savedName);
-<<<<<<< HEAD
-
-=======
->>>>>>> 752249b20df14879e5a4c2eacc49b3cd341a2b6e
 		} catch (IllegalStateException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
